8010bff9b8d4ad6facd4ab7905ab4fa7e37f2954
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-29 14:22:23 -0300

    Arroba kkkkkk

--- source/Main.hx
+++ source/Main.hx
@@ -1,14 +1,11 @@
 package;
 
-import openfl.system.System;
-import openfl.utils.AssetCache;
 import cpp.vm.Gc;
-import flixel.graphics.FlxGraphic;
 import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
-import openfl.Assets;
+import flixel.graphics.FlxGraphic;
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
@@ -16,18 +13,16 @@ import openfl.events.Event;
 
 class Main extends Sprite
 {
-	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	var gameWidth:Int = 1280;
+	var gameHeight:Int = 720;
+	var initialState:Class<FlxState> = TitleState;
+	var zoom:Float = -1;
+	var framerate:Int = 60;
+	var skipSplash:Bool = true;
+	var startFullscreen:Bool = false;
+
 	public static var fpsCounter:FPS;
 	public static var fpsVar:FPS;
-	
-
-	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
@@ -39,25 +34,18 @@ class Main extends Sprite
 		super();
 
 		if (stage != null)
-		{
 			init();
-		}
 		else
-		{
 			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
 	}
 
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
 			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
 
 		setupGame();
 	}
-	
 
 	private function setupGame():Void
 	{
@@ -76,88 +64,73 @@ class Main extends Sprite
 		#if !debug
 		initialState = TitleState;
 		#end
-		
+
 		ClientPrefs.startControls();
 
-		#if cpp 
+		#if cpp
 		Gc.enable(true);
 		#end
 
-		// Paths.getModFolders();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
 		FlxGraphic.defaultPersist = false;
 
 		FlxG.signals.gameResized.add(onResizeGame);
-		// FlxG.signals.preStateSwitch.add(function () {
-		// 	Paths.clearStoredMemory(true);
-		// 	FlxG.bitmap.dumpCache();
 
-		// 	var cache = cast(Assets.cache, AssetCache);
-		// 	for (key=>font in cache.font)
-		// 		cache.removeFont(key);
-		// 	for (key=>sound in cache.sound)
-		// 		cache.removeSound(key);
-
-		// 	gc();
-		// });
-		// FlxG.signals.postStateSwitch.add(function () {
-		// 	Paths.clearUnusedMemory();
-		// 	gc();
-
-		// 	trace(System.totalMemory);
-		// });
-		
-		#if !mobile
+		#if desktop
 		fpsCounter = new FPS(10, 5, 0xFFFFFF);
 		addChild(fpsCounter);
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
-		if(fpsCounter != null) { 
+		if (fpsCounter != null)
 			fpsCounter.visible = ClientPrefs.showFPS;
-		}
 		#end
-
-		trace(FlxG.save.path)
-		;
-
-		
 
 		#if html5
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
 		#end
+
+		#if mobile
+		FlxG.autoPause = false;
+		#end
 	}
 
-	function onResizeGame(w:Int, h:Int) {
+	function onResizeGame(w:Int, h:Int)
+	{
 		if (FlxG.cameras == null)
 			return;
 
-		for (cam in FlxG.cameras.list) {
+		for (cam in FlxG.cameras.list)
+		{
 			@:privateAccess
-			if (cam != null && (cam.filters != null || cam.filters != []))
+			if (cam != null && cam.filters != null && cam.filters.length > 0)
 				fixShaderSize(cam);
-		}	
-		
+		}
 	}
 
-	function fixShaderSize(camera:FlxCamera) // Shout out to Ne_Eo for bringing this to my attention
+	function fixShaderSize(camera:FlxCamera)
+	{
+		@:privateAccess
 		{
-			@:privateAccess {
-				var sprite:Sprite = camera.flashSprite;
-	
-				if (sprite != null)
-				{
-					sprite.__cacheBitmap = null;
-					sprite.__cacheBitmapData = null;
-					sprite.__cacheBitmapData2 = null;
-					sprite.__cacheBitmapData3 = null;
-					sprite.__cacheBitmapColorTransform = null;
-				}
+			var sprite:Sprite = camera.flashSprite;
+
+			if (sprite != null)
+			{
+				sprite.__cacheBitmap = null;
+				sprite.__cacheBitmapData = null;
+				sprite.__cacheBitmapData2 = null;
+				sprite.__cacheBitmapData3 = null;
+				sprite.__cacheBitmapColorTransform = null;
 			}
 		}
+	}
 
-	public function toggleFPS(fpsEnabled:Bool):Void {
-		fpsCounter.visible = fpsEnabled;
+	public function toggleFPS(fpsEnabled:Bool):Void
+	{
+		#if desktop
+		if (fpsCounter != null)
+			fpsCounter.visible = fpsEnabled;
+		#end
 	}
 
 	public function setFPSCap(cap:Float)
@@ -172,10 +145,15 @@ class Main extends Sprite
 
 	public function getFPS():Float
 	{
-		return fpsCounter.currentFPS;
+		#if desktop
+		if (fpsCounter != null)
+			return fpsCounter.currentFPS;
+		#end
+		return 0;
 	}
 
-	public static function gc() {
+	public static function gc()
+	{
 		#if cpp
 		Gc.run(true);
 		#else
